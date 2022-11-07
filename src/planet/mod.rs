cb963798ca1475b42723999b67e5ea8c7b75c707
dylan <dylan_hanley@protonmail.com>
2022-11-07 02:01:33 +0000

    beep

--- src/planet/mod.rs
+++ src/planet/mod.rs
@@ -8,11 +8,12 @@ use crate::graphics;
 #[derive(Copy, Clone)]
 pub struct CellData {
     pub height: f32,
-    pub vegetation: f32
+    pub humidity: f32,
+    pub temperature: f32
 }
-glium::implement_vertex!(CellData,height,vegetation);
+glium::implement_vertex!(CellData,height,humidity,temperature);
 
-struct PlanetMesh{
+struct PlanetBuffers{
     shape_data :glium::VertexBuffer<graphics::PosNorm>,
     planet_data: glium::VertexBuffer<CellData>,
     indices: glium::IndexBuffer<u32>,
@@ -55,37 +56,46 @@ impl PlanetCells{
         vertices.iter()
             .map(
                 |v|
-                octive_noise(perlin, &v, 2.5, 10, 0.6, 2.5)
+                octive_noise(perlin, &v, 2.5, 7, 0.6, 2.5)
             )
             .collect();
 
-        let veg:Vec<f32> = 
+        let humid:Vec<f32> = 
         vertices.iter()
             .map(
-                |v|
-                octive_noise(perlin, &v, 1.0, 10, 0.6, 2.5)
+                |h|
+                octive_noise(perlin, &(h+glm::vec3(0.0,100.0,0.0)), 2.25, 5, 0.55, 2.5)
             )
             .collect();
 
         PlanetCells{
             height,
-            temperature: vec![0.0;vertices.len()],
-            humidity: veg
+            temperature: vec![0.5;vertices.len()],
+            humidity: humid
         }
     }
     //puts cell data into format usable for buffer
     fn get_cell_data(&self)->Vec<CellData>{
-        self.height.iter()
-            .zip(self.humidity.iter())
-            .map(|c| CellData { height: *c.0, vegetation: *c.1 })
-            .collect()
+        //data in form usable for buffer
+        let mut celldata= Vec::with_capacity(self.height.len());
+        
+        for i in 0..self.height.len(){
+            celldata.push(
+                CellData{
+                    height: self.height[i],
+                    humidity: self.humidity[i],
+                    temperature: self.temperature[i]
+                }
+            );
+        }
+        celldata
     }
 }
 
 pub struct Planet{
     texture: glium::texture::SrgbTexture2d,
 
-    mesh: PlanetMesh,
+    mesh: PlanetBuffers,
 
     cells: PlanetCells,
 
@@ -117,7 +127,7 @@ impl Planet{
             texture,
 
             mesh: 
-            PlanetMesh{
+            PlanetBuffers{
                 //buffer containing base shape of planet, most likely the sphere
                 shape_data: glium::VertexBuffer::new(display,&planet_vertices).unwrap(),
                 //buffer containing cell data needed for rendering, dynamic as this will change frequently
@@ -135,7 +145,17 @@ impl Planet{
     }
 
     pub fn update(&mut self, days: f32){
-        self.to_sun= glm::rotate_y_vec3(&self.to_sun, days)
+        //calc tempteretures 
+        //latitude that gets maximum sunlight from the sun
+        let sun_max = glm::dot(&self.to_sun, &self.axis);
+        self.cells.temperature = self.cells.temperature.iter()
+            .map(|t| t-0.001)
+            .collect();
+
+        self.to_sun= glm::rotate_y_vec3(&self.to_sun, days);
+        
+
+        self.mesh.planet_data.write(&self.cells.get_cell_data());
         //self.mesh.vertices.write();
     }
 
